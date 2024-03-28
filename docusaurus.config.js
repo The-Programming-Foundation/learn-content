abdbfda0d2cdf5e83bfdc523b613bb1a66e3490d
Subhajeet Mukherjee <33711869+bootkernel@users.noreply.github.com>
2024-03-28 23:05:08 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'The Programming Foundation',
   tagline: 'We believe that, everybody, regardless of their age, disabilities or educational background, should learn operating systems and programming. We focus on education that is based on written instruction and concise documentation.',
-  url: 'https://learn.theprogrammingfoundation.org/',
-  baseUrl: '/',
+  url: 'https://the-programming-foundation.github.io/',
+  baseUrl: '/tutorials/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
